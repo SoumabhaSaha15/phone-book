896a6fa3857b275cfb5323dc403114c8056596e1
S.Saha <soumabhasaha1509+github@gmail.com>
2024-11-02 08:33:10 +0530

    readme updated.

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import PhoneBook from "./lib/index.js";
 import figlet from "figlet";
 try {
   console.clear();
-  chalkAnimation.rainbow(figlet.textSync("Wellcome to phone-book.",{
-    font:"Banner",
-    whitespaceBreak:true
-  }));
+  PhoneBook.exportToVirtualContactFile();
+  // chalkAnimation.rainbow(figlet.textSync("Wellcome to phone-book.",{
+  //   font:"Banner",
+  //   whitespaceBreak:true,
+  // }));
 } catch (error) {
   console.error(error);
 }
