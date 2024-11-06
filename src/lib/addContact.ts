1295b4fd8b56b98a22c64073f12fa873820eca91
S.Saha <soumabhasaha1509+github@gmail.com>
2024-11-07 00:51:44 +0530

    inquerer.js in add contact

--- src/lib/addContact.ts
+++ src/lib/addContact.ts
@@ -1,107 +1,233 @@
+import inquirer from "inquirer";
 import chalk from "chalk";
 import { Contact, ContactValidator, addressObject } from "../db/schema.js";
 import db from "../db/index.js";
-import PS from "prompt-sync";
 import chalkAnimation from "chalk-animation";
 import { z, ZodError } from "zod"
 import { eq } from "drizzle-orm";
 const addContact = async (): Promise<void> => {
-  const prompt = PS();
-
-  const getEmail = (): null | string | undefined => {
-    const res = prompt(chalk.yellow("Want to add an email (Y/N): "));
-    if (res === 'Y' || res === 'N') {
-      return res === 'Y' ?
-        (prompt(chalk.yellow("Enter email: ")) || "") :
-        null;
-    } else {
-      console.log(chalk.red.bold("Process terminated due to invalid option input."))
-      process.exit(0);
-    }
-  };
-
-  const getMiddleName = (): null | string | undefined => {
-    const res = prompt(chalk.yellow("Want to add an middle name (Y/N): "));
-    if (res === 'Y' || res === 'N') {
-      return res === 'Y' ?
-        (prompt(chalk.yellow("Enter middle name: ")) || "") :
-        null;
-    } else {
-      console.log(chalk.red.bold("Process terminated due to invalid option input."))
-      process.exit(0);
-    }
-  };
-
-  const getBirthday = (): null | string | undefined => {
-    const res = prompt(chalk.yellow("Want to add an birthday (Y/N): "));
-    if (res === 'Y' || res === 'N') {
-      return res === 'Y' ?
-        (prompt(chalk.yellow("Enter birthday(YYYY-MM-DD): ")) || "") :
-        null;
-    } else {
-      console.log(chalk.red.bold("Process terminated due to invalid option input."))
-      process.exit(0);
-    }
-  };
 
 
-
-
-
-  const getAddress = (): null | z.infer<typeof addressObject> | undefined => {
-    const getAddressObject = (): z.infer<typeof addressObject> | undefined => {
-      let address_object:any = { city: "", countryRegion: "", label: "", postalCode: "", stateProvince: "", street: "" };
-      for (const property in address_object)
-        address_object[property] = prompt(`Please enter the ${property}: `);
-      const parseResult = addressObject.safeParse(address_object);
-      if (parseResult.success) {
-        return parseResult.data
-      } else {
-        console.log(chalk.red.bold(JSON.stringify(parseResult.error, null, "\t")));
-        process.exit(0);
+  let getData: z.infer<typeof ContactValidator> = await inquirer.prompt<z.infer<typeof ContactValidator>>([
+    {
+      type: "input",
+      name: "firstName",
+      message: chalk.yellow.bold("Enter your firstName: "),
+      validate: (value) => {
+        const firstNameValidator = ContactValidator.pick({ firstName: true });
+        if (typeof value !== 'string')
+          return "you need to provide a string";
+        else {
+          const validation = firstNameValidator.safeParse({ firstName: value });
+          return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+        }
       }
-    }
-    const res = prompt(chalk.yellow("Want to add an address (Y/N): "));
-    if (res === 'Y' || res === 'N') {
-      return res === 'Y' ?
-        getAddressObject() :
-        null;
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: chalk.yellow.bold("Enter your lastName: "),
+      validate: (value) => {
+        const lastNameValidator = ContactValidator.pick({ lastName: true });
+        if (typeof value !== 'string')
+          return "you need to provide a string";
+        else {
+          const validation = lastNameValidator.safeParse({ lastName: value });
+          return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "phoneNumber",
+      message: chalk.yellow.bold("Enter your phoneNumber: "),
+      validate: (value) => {
+        const phoneNumberValidator = ContactValidator.pick({ phoneNumber: true });
+        if (typeof value !== 'string')
+          return "you need to provide a string";
+        else {
+          const validation = phoneNumberValidator.safeParse({ phoneNumber: value });
+          return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "middleName",
+      message: chalk.yellow.bold("Enter your middleName(press enter to skip): "),
+      validate: (value) => {
+        const middleNameValidator = ContactValidator.pick({ middleName: true });
+        if (typeof value !== 'string')
+          return "you need to provide a string";
+        else {
+          const validation = middleNameValidator.safeParse({ middleName: (value == '' ? null : value) });
+          return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: chalk.yellow.bold("Enter your email(press enter to skip): "),
+      validate: (value) => {
+        const emailValidator = ContactValidator.pick({ email: true });
+        if (typeof value !== 'string')
+          return "you need to provide a string";
+        else {
+          const validation = emailValidator.safeParse({ email: (value == '' ? null : value) });
+          return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "birthday",
+      message: chalk.yellow.bold("Enter your birthday[YYYY-MM-DD](press enter to skip): "),
+      validate: (value) => {
+        const birthdayValidator = ContactValidator.pick({ birthday: true });
+        if (typeof value !== 'string')
+          return "you need to provide a string";
+        else {
+          const validation = birthdayValidator.safeParse({ birthday: (value == '' ? null : value) });
+          return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+        }
+      }
+    },
+  ]);
+  
+  inquirer.prompt([{
+    type: "list",
+    name: "confirm",
+    message: chalk.yellow.bold("Do you want to add an address??: "),
+    choices: ['Yes', 'No'],
+    default: 'No'
+  }]).then(async (data) => {
+    
+    if (data.confirm === 'Yes') {
+      getData.address = await inquirer.prompt<z.infer<typeof addressObject>>([
+        {
+          type: "input",
+          name: "city",
+          message: chalk.yellow.bold("Enter your city: "),
+          validate: (value) => {
+            const cityValidator = addressObject.pick({ city: true });
+            if (typeof value !== 'string')
+              return "you need to provide a string";
+            else {
+              const validation = cityValidator.safeParse({ city: value });
+              return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+            }
+          }
+        },
+        {
+          type: "input",
+          name: "countryRegion",
+          message: chalk.yellow.bold("Enter your countryRegion: "),
+          validate: (value) => {
+            const countryRegionValidator = addressObject.pick({ countryRegion: true });
+            if (typeof value !== 'string')
+              return "you need to provide a string";
+            else {
+              const validation = countryRegionValidator.safeParse({ countryRegion: value });
+              return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+            }
+          }
+        },
+        {
+          type: "input",
+          name: "label",
+          message: chalk.yellow.bold("Enter your label: "),
+          validate: (value) => {
+            const labelValidator = addressObject.pick({ label: true });
+            if (typeof value !== 'string')
+              return "you need to provide a string";
+            else {
+              const validation = labelValidator.safeParse({ label: value });
+              return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+            }
+          }
+        },
+        {
+          type: "input",
+          name: "postalCode",
+          message: chalk.yellow.bold("Enter your postalCode: "),
+          validate: (value) => {
+            const postalCodeValidator = addressObject.pick({ postalCode: true });
+            if (typeof value !== 'string')
+              return "you need to provide a string";
+            else {
+              const validation = postalCodeValidator.safeParse({ postalCode: value });
+              return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "stateProvince",
+          message: chalk.yellow.bold("Enter your state Province: "),
+          validate: (value) => {
+            const stateProvinceValidator = addressObject.pick({ stateProvince: true });
+            if (typeof value !== 'string')
+              return "you need to provide a string";
+            else {
+              const validation = stateProvinceValidator.safeParse({ stateProvince: value });
+              return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+            }
+          }
+        },
+        {
+          type: "input",
+          name: "street",
+          message: chalk.yellow.bold("Enter your street: "),
+          validate: (value) => {
+            const streetValidator = addressObject.pick({ street: true });
+            if (typeof value !== 'string')
+              return "you need to provide a string";
+            else {
+              const validation = streetValidator.safeParse({ street: value });
+              return (validation.success) ? (true) : (JSON.stringify(validation.error, null, "\t"));
+            }
+          }
+        },
+      ]);
     } else {
-      console.log(chalk.red.bold("Process terminated due to invalid option input."))
-      process.exit(0);
+      getData.address = null;
     }
-  };
 
-
-  const getData: z.infer<typeof ContactValidator> = {
-    firstName: prompt(chalk.yellow("Enter first name: ")) || "",
-    middleName: getMiddleName(),
-    lastName: prompt(chalk.yellow("Enter last name: ")) || "",
-    phoneNumber: prompt(chalk.yellow("Enter phone-number: ")) || "",
-    email: getEmail(),
-    birthday: getBirthday(),
-    address: getAddress()
-  };
-
-  try {
-    ContactValidator.parse(getData);
-    try {
-      const stringified_address = getData.address?(JSON.stringify(getData.address)):(null);
-      const result = await db.insert(Contact).values({...getData,address:stringified_address}).execute();
-      const lastInserted = ((await db.select().from(Contact).where(eq(Contact.id, result.lastInsertRowid as number))))[0];
-      console.clear();
-      const rainbow = chalkAnimation.rainbow(`Contact successfully added, id: ${result.lastInsertRowid}.`);
-      console.log(rainbow);
-      console.log(chalk.bgBlack.greenBright.bold(`${JSON.stringify(lastInserted, null, "\t")}.`));
-      setTimeout(()=>{
-        rainbow.stop();
-      },2000);
-    } catch (err) {
-      console.log(chalk.red.bold(JSON.stringify(err, null, "\t")));
+    if (getData?.middleName === '') {
+      getData.middleName = null
     }
-  } catch (error) {
-    let err = (error as ZodError);
-    console.log(chalk.red.bold(JSON.stringify(err, null, "\t")))
-  }
+    if (getData?.email === '') {
+      getData.email = null;
+    }
+    if (getData.birthday === '') {
+      getData.birthday = null;
+    }
+    console.table(getData);
+  });
+
+  
+
+
+  // try {
+  //   ContactValidator.parse(getData);
+  //   try {
+  //     const stringified_address = getData.address ? (JSON.stringify(getData.address)) : (null);
+  //     const result = await db.insert(Contact).values({ ...getData, address: stringified_address }).execute();
+  //     const lastInserted = ((await db.select().from(Contact).where(eq(Contact.id, result.lastInsertRowid as number))))[0];
+  //     console.clear();
+  //     const rainbow = chalkAnimation.rainbow(`Contact successfully added, id: ${result.lastInsertRowid}.`);
+  //     console.log(rainbow);
+  //     console.log(chalk.bgBlack.greenBright.bold(`${JSON.stringify(lastInserted, null, "\t")}.`));
+  //     setTimeout(() => {
+  //       rainbow.stop();
+  //     }, 2000);
+  //   } catch (err) {
+  //     console.log(chalk.red.bold(JSON.stringify(err, null, "\t")));
+  //   }
+  // } catch (error) {
+  //   let err = (error as ZodError);
+  //   console.log(chalk.red.bold(JSON.stringify(err, null, "\t")))
+  // }
 };
 export default addContact;
