a9f95f96136dfe2945ba7ac63b5a0292d9c644dd
S.Saha <soumabhasaha1509+github@gmail.com>
2024-12-28 22:42:41 +0530

    some dependency updated

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 import inquirer from "inquirer";
 import figlet from "figlet";
+import { rainbow } from "gradient-string";
 import Menu, { MenuString, Commands, CommandString } from "./lib/menu.js";
 console.clear();
 try {
@@ -20,7 +21,7 @@ try {
     setTimeout(startProcess, 2000);
   } else {
     if (Object.keys(Commands).includes(process.argv[2]) && process.argv.length === 3) {
-      console.log(chalk.blue(figlet.textSync(process.argv[2].toLocaleUpperCase(), { whitespaceBreak: true })));
+      console.log(rainbow(figlet.textSync(process.argv[2].toLocaleUpperCase(), { whitespaceBreak: true })));
       Commands[process.argv[2] as CommandString]();
     } else throw new Error(chalk.red(figlet.textSync("\nInvalid command", { whitespaceBreak: true })));
   }
